7cd7117bff9cb23f6599f80b73f3409019d99641
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:09:28 +0530

    027

---
+++ pro027/pro027/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    readonly int x = 10;
+    const double pi = 3.14;
+
+    static void Main()
+    {
+        Program obj = new Program();
+        Console.WriteLine(obj.x);
+        Console.WriteLine(pi);
+    }
+}
---
+++ pro027/pro027/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pro027")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pro027")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36d7906d-c9b9-49cf-a905-49c46c2ae74c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
