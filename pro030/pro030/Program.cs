0d936300440c61ea685adc0f38a5c15e53cae19d
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:14:10 +0530

    030

---
+++ pro030/pro030/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int choice;
+
+        Console.Write("Enter number (1-3): ");
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("One");
+                break;
+            case 2:
+                Console.WriteLine("Two");
+                break;
+            case 3:
+                Console.WriteLine("Three");
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+}
