cd7f2dbfaa158e58c741ddbd245b25c1994ac5ac
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:12:49 +0530

    029

---
+++ pro029/pro029/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+            Console.WriteLine("Positive");
+        else
+            Console.WriteLine("Negative");
+    }
+}
