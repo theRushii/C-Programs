02f54f149600284fa5db5e07db3a85ed30c615c9
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:37:48 +0530

    016

---
+++ pro016/pro016/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, sum = 0;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (num != 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+
+        Console.WriteLine("Sum of digits = " + sum);
+    }
+}
