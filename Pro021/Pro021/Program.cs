8a594426bd69d978f4ba6255709f4eb300106c17
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:45:10 +0530

    021

---
+++ Pro021/Pro021/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, gcd = 1, lcm;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= a && i <= b; i++)
+            if (a % i == 0 && b % i == 0)
+                gcd = i;
+
+        lcm = (a * b) / gcd;
+
+        Console.WriteLine("GCD = " + gcd);
+        Console.WriteLine("LCM = " + lcm);
+    }
+}
