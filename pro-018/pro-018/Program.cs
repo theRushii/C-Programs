76152893bae7387eafd021161e5623bad2c4f9c7
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:40:25 +0530

    018

---
+++ pro-018/pro-018/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+
+        Console.Write("Enter number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+            Console.WriteLine(n + " x " + i + " = " + n * i);
+    }
+}
