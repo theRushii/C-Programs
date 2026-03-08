2d8a51a9e658334f9b22ac50625e8ddc9f9547d4
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:54:40 +0530

    022

---
+++ pro022/pro022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double c, f;
+
+        Console.Write("Enter Celsius: ");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        f = (c * 9 / 5) + 32;
+
+        Console.WriteLine("Fahrenheit = " + f);
+    }
+}
