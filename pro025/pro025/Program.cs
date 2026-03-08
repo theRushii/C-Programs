ebea108ab4fda75d20ac822ba1482a861f4718df
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:06:32 +0530

    025

---
+++ pro025/pro025/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char ch;
+
+        Console.Write("Enter character: ");
+        ch = Convert.ToChar(Console.ReadLine());
+
+        int ascii = ch;
+
+        Console.WriteLine("ASCII value = " + ascii);
+    }
+}
