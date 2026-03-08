b1f6b6da455173fe85c22021b14a7fb530c4911a
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:36:33 +0530

    015

---
+++ pro015/pro015/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, sum = 0, temp;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        temp = num;
+
+        while (num != 0)
+        {
+            int rem = num % 10;
+            sum = sum + rem * rem * rem;
+            num = num / 10;
+        }
+
+        if (sum == temp)
+            Console.WriteLine("Armstrong number");
+        else
+            Console.WriteLine("Not Armstrong number");
+    }
+}
