597a2734a0a2c4e18861bc62ecf7fe7cd9b43509
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:29:21 +0530

    013

---
+++ pro013/pro013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, rev = 0;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (num != 0)
+        {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+
+        Console.WriteLine("Reversed number = " + rev);
+    }
+}
