b70cab04b3bae63e34b7ddf4473a190dc9c6eb39
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 15:51:55 +0530

    Program003

---
+++ Program003/Program003/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, sum;
+
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        sum = num1 + num2;
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
