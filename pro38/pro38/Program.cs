a72899fa798be2b6e5f90cc50e4a9ad88764f71d
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:31:34 +0530

    038

---
+++ pro38/pro38/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int marks;
+
+        Console.Write("Enter marks: ");
+        marks = Convert.ToInt32(Console.ReadLine());
+
+        if (marks >= 80)
+            Console.WriteLine("Grade A");
+        else if (marks >= 60)
+            Console.WriteLine("Grade B");
+        else if (marks >= 40)
+            Console.WriteLine("Grade C");
+        else
+            Console.WriteLine("Fail");
+    }
+}
