5bf431d0bf9e29c11ed588ae67eb473f676ff0d5
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:36:31 +0530

    040

---
+++ pro40/pro40/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, sum = 0;
+
+        Console.Write("Enter number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+            sum = sum + i;
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
