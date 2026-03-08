f50844ba7caa729a6b6d91ca27a7f42581d565c6
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:21:41 +0530

    009

---
+++ pro009/pro009/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b && a > c)
+            Console.WriteLine("Largest number is: " + a);
+        else if (b > c)
+            Console.WriteLine("Largest number is: " + b);
+        else
+            Console.WriteLine("Largest number is: " + c);
+    }
+}
