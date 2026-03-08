27da4209e713021720edd8ff411b18765721a1ed
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:57:40 +0530

    023

---
+++ pro023/pro023/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double r, area;
+
+        Console.Write("Enter radius: ");
+        r = Convert.ToDouble(Console.ReadLine());
+
+        area = 3.14 * r * r;
+
+        Console.WriteLine("Area of circle = " + area);
+    }
+}
