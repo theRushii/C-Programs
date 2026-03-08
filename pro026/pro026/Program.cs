c02dbf26773653d2ed6f3084984fc5ab52f5d99c
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:07:56 +0530

    026

---
+++ pro026/pro026/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        double b = a;
+        double c = 9.78;
+        int d = (int)c;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+    }
+}
