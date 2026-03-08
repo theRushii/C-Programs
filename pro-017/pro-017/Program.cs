b374dd1f5da2dea59be580f8ce81964981ed211a
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:39:08 +0530

    017

---
+++ pro-017/pro-017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, count = 0;
+
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (num != 0)
+        {
+            num = num / 10;
+            count++;
+        }
+
+        Console.WriteLine("Number of digits = " + count);
+    }
+}
