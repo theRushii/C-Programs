eab434e87a25b2bffe20a0cbba6a3044bb936018
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:23:52 +0530

    034

---
+++ pro034/pro034/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i == 5)
+                continue;
+
+            if (i == 9)
+                break;
+
+            Console.WriteLine(i);
+        }
+    }
+}
