0add263456bb531fc2b8f0da9ced71daf478e31c
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:40:32 +0530

    042

---
+++ pro042/pro042/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int days, years, months, remDays;
+
+        Console.Write("Enter days: ");
+        days = Convert.ToInt32(Console.ReadLine());
+
+        years = days / 365;
+        months = (days % 365) / 30;
+        remDays = (days % 365) % 30;
+
+        Console.WriteLine("Years = " + years);
+        Console.WriteLine("Months = " + months);
+        Console.WriteLine("Days = " + remDays);
+    }
+}
