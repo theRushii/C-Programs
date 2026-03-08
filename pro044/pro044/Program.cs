1972c794716fbad6b4a3dae791f8d0bac9529cc7
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:43:23 +0530

    044

---
+++ pro044/pro044/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = 10;
+        var b = "Hello";
+
+        dynamic x = 20;
+        x = "Dynamic variable";
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(x);
+    }
+}
