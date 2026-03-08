3e6528094ae34755a491d56f569bc453dfa302e2
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:41:53 +0530

    019

---
+++ pro019/pro019/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int start, end;
+
+        Console.Write("Enter start: ");
+        start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter end: ");
+        end = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++)
+                if (i % j == 0)
+                    count++;
+
+            if (count == 2)
+                Console.WriteLine(i);
+        }
+    }
+}
