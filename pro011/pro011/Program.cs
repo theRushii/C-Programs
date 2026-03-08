b1d667fbac9cda910ac11055c3df692f15eb8a47
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:26:13 +0530

    011

---
+++ pro011/pro011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, fact = 1;
+
+        Console.Write("Enter a number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+            fact = fact * i;
+
+        Console.WriteLine("Factorial = " + fact);
+    }
+}
