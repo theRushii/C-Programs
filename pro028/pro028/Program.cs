96476f4e48ae5c7e0a2c206fbac470e32766d668
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:10:52 +0530

    028

---
+++ pro028/pro028/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10, b = 5;
+
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+        Console.WriteLine(a % b);
+    }
+}
