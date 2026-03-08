df6a91f3b13ea2151d7218a07812808cb21d38b0
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:30:08 +0530

    037

---
+++ pro037/pro037/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int baseNum, exp;
+        double result;
+
+        Console.Write("Enter base: ");
+        baseNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter exponent: ");
+        exp = Convert.ToInt32(Console.ReadLine());
+
+        result = Math.Pow(baseNum, exp);
+
+        Console.WriteLine("Result = " + result);
+    }
+}
