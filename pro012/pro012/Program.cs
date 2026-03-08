31d2e2ff78b90729eeb70e26485e3d82dc1e4b00
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:27:50 +0530

    012

---
+++ pro012/pro012/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, a = 0, b = 1, c;
+
+        Console.Write("Enter number of terms: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
