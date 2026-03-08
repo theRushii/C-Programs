2c59cdcdba96d71dba6e1c5c02f64f76e751be87
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:38:50 +0530

    041

---
+++ Pro041/Pro041/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int year;
+
+        Console.Write("Enter year: ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            Console.WriteLine("Leap Year");
+        else
+            Console.WriteLine("Not Leap Year");
+    }
+}
