b915de9ccc2ec39bf75d490b6e6a9355d9383bfc
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:34:04 +0530

    039

---
+++ pro039/pro039/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double basic, hra, da, total;
+
+        Console.Write("Enter basic salary: ");
+        basic = Convert.ToDouble(Console.ReadLine());
+
+        hra = basic * 0.2;
+        da = basic * 0.1;
+        total = basic + hra + da;
+
+        Console.WriteLine("Total salary = " + total);
+    }
+}
