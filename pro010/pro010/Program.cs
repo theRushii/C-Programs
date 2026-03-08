3c7334d6de175fd8ad3d9cd8530189a113d4b3dc
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:23:19 +0530

    010

---
+++ pro010/pro010/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, temp;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("After swapping:");
+        Console.WriteLine("First number: " + a);
+        Console.WriteLine("Second number: " + b);
+    }
+}
