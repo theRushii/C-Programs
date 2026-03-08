7db57ea2acfc5d6a914174f3fe772266fe678c7a
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:05:04 +0530

    PRO007

--- ConsoleApp3/ConsoleApp3/Properties/AssemblyInfo.cs
+++ ConsoleApp3/ConsoleApp3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Program006")]
+[assembly: AssemblyTitle("ConsoleApp3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Program006")]
+[assembly: AssemblyProduct("ConsoleApp3")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("da7d6cfc-4ddd-4bd1-85e2-e477007df9d3")]
+[assembly: Guid("2a0204ff-a326-4276-a9ae-efc2f23d0080")]
 
 // Version information for an assembly consists of the following four values:
 //
