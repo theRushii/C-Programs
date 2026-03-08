48c21d67f96a3c499894c5408db6f924f2d53e3b
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:43:31 +0530

    020

---
+++ pro020/pro020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, count = 0;
+
+        Console.Write("Enter number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+            if (n % i == 0)
+                count++;
+
+        if (count == 2)
+            Console.WriteLine("Prime number");
+        else
+            Console.WriteLine("Not prime");
+    }
+}
