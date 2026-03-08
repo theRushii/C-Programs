514833967e740becafd6f4eb62a1895bffd6a739
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:59:39 +0530

    024

---
+++ pro024/pro024/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double l, b, area;
+
+        Console.Write("Enter length: ");
+        l = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter breadth: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        area = l * b;
+
+        Console.WriteLine("Area of rectangle = " + area);
+    }
+}
