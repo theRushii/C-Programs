90b1fb8498c9375a8171fc382057687942a9afa7
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:44:36 +0530

    045

---
+++ pro045/pro045/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
+class Program
+{
+    static void Main()
+    {
+        Days d = Days.Wednesday;
+        Console.WriteLine(d);
+    }
+}
