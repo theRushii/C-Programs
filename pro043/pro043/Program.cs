2153b3f082a57a95f65eccbb9d09e4244b8fca47
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:42:25 +0530

    043

---
+++ pro043/pro043/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int? num = null;
+
+        if (num.HasValue)
+            Console.WriteLine(num.Value);
+        else
+            Console.WriteLine("Value is null");
+
+        num = 25;
+        Console.WriteLine(num);
+    }
+}
