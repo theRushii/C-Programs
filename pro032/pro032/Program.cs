bfc1a02e58b58f3e011ea5753fe8177e8d877e47
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 17:21:12 +0530

    032

---
+++ pro032/pro032/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
