3fc7a12d77d653dca79817878b97d7bfacc9302c
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 15:56:40 +0530

    Pro004

---
+++ Program004/Program004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b)
+            Console.WriteLine("Maximum number is: " + a);
+        else
+            Console.WriteLine("Maximum number is: " + b);
+    }
+}
--- Program004/Program004/Properties/AssemblyInfo.cs
+++ Program004/Program004/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Program003")]
+[assembly: AssemblyTitle("Program004")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Program003")]
+[assembly: AssemblyProduct("Program004")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1c0be22-bf0c-4136-b370-bd769cf2cd6a")]
+[assembly: Guid("abc92fc4-a449-4d55-8cf2-79e5399609a5")]
 
 // Version information for an assembly consists of the following four values:
 //
