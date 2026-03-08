71ea2a4c04dbdcee5ee4909be2e197c1b39b56f5
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-08 16:13:42 +0530

    c#program

---
+++ Program005/Program005/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+            Console.WriteLine("Number is Even");
+        else
+            Console.WriteLine("Number is Odd");
+    }
+}
---
+++ Program006/Program006/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("Number is Positive");
+        else
+            Console.WriteLine("Number is Negative");
+    }
+}
