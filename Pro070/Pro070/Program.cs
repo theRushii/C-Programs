6f3bf261052fc958ca160a7675e221dd0319b5d0
Rushikesh Deshpande <rushikeshdeshpande033@gmail.com>
2026-03-15 22:02:34 +0530

    Pro070

---
+++ Pro070/Pro070/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string a, b;
+
+        Console.Write("Enter first string: ");
+        a = Console.ReadLine();
+
+        Console.Write("Enter second string: ");
+        b = Console.ReadLine();
+
+        if (a == b)
+            Console.WriteLine("Strings are equal");
+        else
+            Console.WriteLine("Strings are not equal");
+    }
+}
